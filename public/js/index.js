4916f1adf7e289d3779ad38d45d29b0b631f2eb0
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-03-21 02:21:57 +0530

    created odd and even row handler functions for handling transform property(moves)

--- public/js/index.js
+++ public/js/index.js
@@ -114,7 +114,6 @@ function createPlayers(n){
   players.push(p)
   
  }
- console.log(players);
 
 
 }
@@ -146,7 +145,7 @@ function Decision() {
   let timer = setTimeout(() => {
     result.src = "./images/dice/dice" + d1 + ".png";
     if (flags_arr[turn] == true) {
-      // oddRowHandler(d1, player, turn);
+      oddRowHandler(d1, player, turn);
       if( d1 == 6){
         turn = turn % players.length;
       }
@@ -156,7 +155,7 @@ function Decision() {
       }
     } 
     else {
-      // evenRowHandler(d1, player, turn);
+      evenRowHandler(d1, player, turn);
       if( d1 == 6){
         turn = turn % players.length;
       }
@@ -168,4 +167,73 @@ function Decision() {
     
     dice_img_div.style.visibility = "visible";
   }, 500);
+}
+
+
+
+// handeling transform property for edd row
+
+function oddRowHandler(moves_right, p, turn) {
+  for (let i = 0; i < moves_right; i++) {
+    pX_arr[turn] = pX_arr[turn] + 3.5;
+
+    if (
+      pX_arr[turn] < 35 &&
+      pX_arr[turn] >= 0 &&
+      pY_arr[turn] <= 0 &&
+      pY_arr[turn] > -35
+    ) {
+      translateHandling(pX_arr[turn], pY_arr[turn], p);
+    } else {
+      pX_arr[turn] = 31.5;
+      pY_arr[turn] = pY_arr[turn] - 3.5;
+
+      translateHandling(pX_arr[turn], pY_arr[turn], p);
+      flags_arr[turn] = !flags_arr[turn];
+      evenRowHandler( (moves_right - i - 1), p, turn);
+      break;
+    }
+  }
+  console.log([pX_arr[turn], pY_arr[turn]]);
+
+}
+
+
+// handeling transform property for even row
+
+
+function evenRowHandler(moves_left, p, turn) {
+  if (pX_arr[turn] <= 21 && pY_arr[turn] == -31.5) {
+    go_to_Home(moves_left, p, turn);
+  }
+   else {
+    for (let i = 0; i < moves_left; i++) {
+      if (
+        pX_arr[turn] < 35 &&
+        pX_arr[turn] > 0 &&
+        pY_arr[turn] <= 0 &&
+        pY_arr[turn] > -35
+      ) {
+        pX_arr[turn] = pX_arr[turn] - 3.5;
+        translateHandling(pX_arr[turn], pY_arr[turn], p)
+      } else {
+        flags_arr[turn] = true;
+        pY_arr[turn] = pY_arr[turn] - 3.5;
+        translateHandling(pX_arr[turn], pY_arr[turn], p)
+        pX_arr[turn] = 0;
+        oddRowHandler(moves_left - i - 1, p, turn);
+        break;
+      }
+    }
+  }
+
+  console.log([pX_arr[turn], pY_arr[turn]]);
+
+}
+
+
+
+
+function translateHandling(pX, pY, player) {
+  player.style.transform = `translate(${pX}rem,${pY}rem)`;
 }
